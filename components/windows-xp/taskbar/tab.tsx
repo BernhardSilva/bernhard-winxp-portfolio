fc844909366c3f7a50261e601514db96911756e7
Bernhard Silva <bernhard.silva@gmail.com>
2023-11-14 06:50:31 -0300

    close page tab active previous page fix

--- components/windows-xp/taskbar/tab.tsx
+++ components/windows-xp/taskbar/tab.tsx
@@ -1,17 +1,24 @@
 import { PageState, usePageStore } from '@/stores/page-store';
-import WindowsCloseButton from '../buttons/windows-close-button';
 import { Icon } from '@iconify/react/dist/iconify.js';
+import WindowsCloseButton from '../buttons/windows-close-button';
 
 type TabProps = {
 	page: PageState;
 };
 
 const Tab = ({ page }: TabProps) => {
-	const { openPage, closePage, toggleMinimizePage, activePageId } = usePageStore((state) => state);
+	const { openPage, closePage, toggleMinimizePage, activePageId, setActivePreviousPage } = usePageStore(
+		(state) => state
+	);
 
 	const handleClick = (id: string) => {
 		openPage(id);
 		if (page.isMinimized) toggleMinimizePage(id);
+	};
+
+	const closeHandler = () => {
+		closePage(page.id);
+		setActivePreviousPage(page.id);
 	};
 
 	return (
@@ -26,7 +33,7 @@ const Tab = ({ page }: TabProps) => {
 		>
 			<Icon icon={page.icon} height={20} width={20} className={`${'hidden md:flex lg:hidden'}`} />
 			<span className='hidden lg:flex'>{page.name}</span>
-			<WindowsCloseButton closeHandler={() => closePage(page.id)} />
+			<WindowsCloseButton closeHandler={closeHandler} />
 		</div>
 	);
 };
