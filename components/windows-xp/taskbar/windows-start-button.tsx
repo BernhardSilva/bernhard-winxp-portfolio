43e9483835b3c78eec2e4260fa0dbdad274a8576
Bernhard Silva <bernhard.silva@gmail.com>
2023-11-14 07:08:59 -0300

    start button extra rounded fix

--- components/windows-xp/taskbar/windows-start-button.tsx
+++ components/windows-xp/taskbar/windows-start-button.tsx
@@ -9,7 +9,7 @@ const WindowsStartButton = ({ handleStartClick }: WindowsStartButton) => {
 	return (
 		<button
 			onClick={handleStartClick}
-			className='flex gap-1 place-items-center px-3 py-2 rounded-md text-white rounded-r-3xl h-[44px] w-28 
+			className='flex gap-1 place-items-center px-3 py-2 text-white rounded-r-3xl h-[44px] w-28 
 			border-r-1 border-t-1 border-green-900
 			bg-gradient-to-br from-green-700 to-green-900 transition duration-500 ease-in-out hover:from-green-500 hover:to-green-700'
 		>
