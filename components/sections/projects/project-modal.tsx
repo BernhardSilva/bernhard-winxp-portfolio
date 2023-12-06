aaa8a3f09b08b7c1fc0a7a5d3806cb7a8026ceaa
Bernhard Silva <bernhard.silva@gmail.com>
2023-12-06 00:27:44 -0300

    skeleton files icon image gallery

--- components/sections/projects/project-modal.tsx
+++ components/sections/projects/project-modal.tsx
@@ -5,7 +5,7 @@ import WindowsCloseButton from '@/components/windows-xp/buttons/windows-close-bu
 import { Project } from '@/types';
 import { Icon } from '@iconify/react/dist/iconify.js';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 
 type ProjectModalProps = {
 	isModalOpen: boolean;
@@ -14,7 +14,7 @@ type ProjectModalProps = {
 };
 
 const ProjectModal: React.FC<ProjectModalProps> = ({ isModalOpen, closeModal, selectedProject }) => {
-
+	const [isLoaded, setIsLoaded] = useState(false);
 	return (
 		<div className='w-full h-full'>
 			{isModalOpen && selectedProject && (
@@ -28,20 +28,20 @@ const ProjectModal: React.FC<ProjectModalProps> = ({ isModalOpen, closeModal, se
 					>
 						<WindowsCloseButton closeHandler={closeModal} className='absolute top-3 right-3' />
 						<div className='bg-white dark:bg-slate-950 rounded-lg p-10'>
-							<ImageSkeleton
-								image={
-									<Image
-										className='w-full object-cover rounded-md h-[200px]'
-										src={selectedProject?.image.modalImage}
-										alt={selectedProject?.title}
-										width={500}
-										height={200}
-									/>
-								}
-								imageLoaded={!!selectedProject?.image.modalImage}
-								width={500}
-								height={200}
-							/>
+							{!isLoaded ? (
+								<div
+									className={`animate-pulse bg-gradient-to-r from-gray-500 to-gray-700 rounded-md w-[${500}px] h-[${200}px]`}
+								></div>
+							) : (
+								<Image
+									className='w-full object-cover rounded-md h-[200px]'
+									src={selectedProject?.image.modalImage}
+									alt={selectedProject?.title}
+									width={500}
+									height={200}
+									onLoad={() => setIsLoaded(true)}
+								/>
+							)}
 
 							<div className='bg-slate-100 dark:bg-slate-800 p-2 rounded-md mt-3'>
 								<h3 className='text-lg mt-2'>{selectedProject?.title}</h3>
