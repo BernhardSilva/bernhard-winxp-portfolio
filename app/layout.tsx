f9202070125debe346fc8a414352d2706bb65f9e
Bernhard Silva <bernhard.silva@gmail.com>
2024-01-03 18:21:44 -0300

    favicon updt

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 import { Toaster } from 'sonner';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,6 +15,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en'>
+			<Head>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 			<body className={inter.className}>
 				{children}
 				<Analytics />
