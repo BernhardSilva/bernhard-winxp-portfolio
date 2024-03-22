a2890d12087df727573fe663045e2b0a25121466
BernhardSilva <bernhard.silva@gmail.com>
2024-03-22 15:05:28 -0300

    loading spinner into contact me button sending email

--- components/sections/contact/form.tsx
+++ components/sections/contact/form.tsx
@@ -1,9 +1,13 @@
+'use client';
+
 import { toastErrorStyle, toastSuccessStyle } from '@/helpers/toasts-style';
 import { Icon } from '@iconify/react/dist/iconify.js';
 import { FormEvent, useState } from 'react';
 import { toast } from 'sonner';
 
 const ContactForm = () => {
+	const [loading, setLoading] = useState(false);
+
 	const formFields = [
 		{
 			name: 'email',
@@ -64,27 +68,39 @@ const ContactForm = () => {
 		}
 	};
 
-	const sendEmail = async () => {
-		const response = await fetch('/api/send', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(formData)
+	const showError = () => {
+		toast.error('Something went wrong!', {
+			style: { ...toastErrorStyle }
 		});
-		if (response.ok) {
-			toast.success('Email sent successfully!', {
-				style: { ...toastSuccessStyle }
+	};
+
+	const sendEmail = async () => {
+		setLoading(true);
+		try {
+			const response = await fetch('/api/send', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(formData)
 			});
-			setFormData({ ...initialFormData });
-			setErrors(initialFormData);
+			if (response.ok) {
+				toast.success('Email sent successfully!', {
+					style: { ...toastSuccessStyle }
+				});
+				setFormData({ ...initialFormData });
+				setErrors(initialFormData);
+				return;
+			}
+			const { error } = await response.json();
+			if (error) {
+				showError();
+			}
+		} catch (error) {
+			showError();
+		} finally {
+			setLoading(false);
 			return;
-		}
-		const { error } = await response.json();
-		if (error) {
-			toast.error('Something went wrong!', {
-				style: { ...toastErrorStyle }
-			});
 		}
 	};
 
@@ -130,15 +146,32 @@ const ContactForm = () => {
 					))}
 					<div className='flex justify-around place-items-center'>
 						<button
+							disabled={loading}
 							type='submit'
-							className='bg-slate-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline transition duration-300 ease-in-out hover:scale-105'
+							className={`flex justify-center items-center bg-slate-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline transition duration-300 ease-in-out hover:scale-105`}
 						>
-							<Icon
-								className='cursor-pointer transition duration-300 ease-in-out hover:scale-110'
-								icon='bi:send-fill'
-								width={25}
-								height={25}
-							/>
+							{loading ? (
+								<svg
+									className='animate-spin h-6 w-6 text-white'
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+								>
+									<circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
+									<path
+										className='opacity-75'
+										fill='currentColor'
+										d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+									></path>
+								</svg>
+							) : (
+								<Icon
+									className='cursor-pointer transition duration-300 ease-in-out hover:scale-110'
+									icon='bi:send-fill'
+									width={25}
+									height={25}
+								/>
+							)}
 						</button>
 						<a className='flex items-center gap-10' href={`https://wa.link/m7zcha`} target='_blank'>
 							<Icon
