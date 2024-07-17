4f3c32bb949b989b1dc2bc1004d489652f1916ff
bernhardSilva <bernhard.silva@gmail.com>
2024-07-17 17:44:28 -0400

    contact me responsive fix

--- components/sections/contact/form.tsx
+++ components/sections/contact/form.tsx
@@ -105,87 +105,87 @@ const ContactForm = () => {
 	};
 
 	return (
-		<div className='flex flex-col items-center opacity-95'>
-		  <h2 className='text-3xl mt-5 text-gray-800 dark:text-white bg-[#C8C8C8] dark:bg-slate-800 rounded-lg p-2 transition duration-500'>
-			Contact Me
-		  </h2>
-		  <div className='flex flex-col border-2 border-[#C8C8C8] dark:border-slate-700 place-content-center justify-center p-8 bg-[#dfdfdf] dark:bg-slate-800 rounded-md mt-5 transition duration-500 w-full max-w-lg'>
-			<form onSubmit={handleSubmit} className='w-full'>
-			  {formFields.map((field, index) => (
-				<div key={index} className='mb-4'>
-				  <label
-					htmlFor={field.name}
-					className='block text-gray-800 dark:text-gray-300 text-sm font-bold mb-2 text-start'
-				  >
-					{field.label}
-				  </label>
-				  {field.type === 'textarea' ? (
-					<textarea
-					  className={`shadow appearance-none border rounded w-full max-h-[150px] py-2 px-3 bg-[#F8F8F8] dark:bg-slate-600 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline ${
-						errors[field.name] && 'border-red-500'
-					  }`}
-					  id={field.name}
-					  name={field.name}
-					  value={formData[field.name]}
-					  onChange={handleInputChange}
-					/>
-				  ) : (
-					<input
-					  type={field.type}
-					  className={`shadow appearance-none border rounded w-full py-2 px-3 bg-[#F8F8F8] dark:bg-slate-600 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline ${
-						errors[field.name] && 'border-red-500'
-					  }`}
-					  id={field.name}
-					  name={field.name}
-					  value={formData[field.name]}
-					  onChange={handleInputChange}
-					/>
-				  )}
-				  {errors[field.name] && <p className='text-red-500 text-xs italic text-start'>{errors[field.name]}</p>}
-				</div>
-			  ))}
-			  <div className='flex flex-col sm:flex-row justify-around items-center gap-4 mt-4'>
-				<button
-				  disabled={loading}
-				  type='submit'
-				  className='flex justify-center items-center bg-slate-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline transition duration-300 ease-in-out hover:scale-105 w-full sm:w-auto'
-				>
-				  {loading ? (
-					<svg
-					  className='animate-spin h-6 w-6 text-white'
-					  xmlns='http://www.w3.org/2000/svg'
-					  fill='none'
-					  viewBox='0 0 24 24'
-					>
-					  <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
-					  <path
-						className='opacity-75'
-						fill='currentColor'
-						d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-					  ></path>
-					</svg>
-				  ) : (
-					<Icon
-					  className='cursor-pointer transition duration-300 ease-in-out hover:scale-110'
-					  icon='bi:send-fill'
-					  width={25}
-					  height={25}
-					/>
-				  )}
-				</button>
-				<a className='flex items-center' href={`https://wa.link/m7zcha`} target='_blank' rel='noopener noreferrer'>
-				  <Icon
-					className='cursor-pointer transition duration-300 ease-in-out hover:scale-110'
-					icon='logos:whatsapp-icon'
-					width={40}
-					height={40}
-				  />
-				</a>
-			  </div>
-			</form>
-		  </div>
+		<div className='relative flex-inline opacity-95 h-auto'>
+			<h2 className='text-3xl mt-5 text-gray-800 dark:text-white bg-[#C8C8C8] dark:bg-slate-800 rounded-lg p-2 transition duration-500'>
+				Contact Me
+			</h2>
+			<div className='flex border-2 border-[#C8C8C8] dark:border-slate-700 place-content-center justify-center p-8 bg-[#dfdfdf] dark:bg-slate-800 rounded-md mt-5 transition duration-500'>
+				<form onSubmit={handleSubmit} className='w-full max-w-md'>
+					{formFields.map((field, index) => (
+						<div key={index} className='mb-4'>
+							<label
+								htmlFor={field.name}
+								className='block text-gray-800 dark:text-gray-300 text-sm font-bold mb-2 text-start'
+							>
+								{field.label}
+							</label>
+							{field.type === 'textarea' ? (
+								<textarea
+									className={`shadow appearance-none border rounded w-full max-h-[150px] py-2 px-3 bg-[#F8F8F8] dark:bg-slate-600 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline ${
+										errors[field.name] && 'border-red-500'
+									}`}
+									id={field.name}
+									name={field.name}
+									value={formData[field.name]}
+									onChange={handleInputChange}
+								/>
+							) : (
+								<input
+									type={field.type}
+									className={`shadow appearance-none border rounded w-full py-2 px-3 bg-[#F8F8F8] dark:bg-slate-600 text-gray-700 dark:text-white leading-tight focus:outline-none focus:shadow-outline ${
+										errors[field.name] && 'border-red-500'
+									}`}
+									id={field.name}
+									name={field.name}
+									value={formData[field.name]}
+									onChange={handleInputChange}
+								/>
+							)}
+							{errors[field.name] && <p className='text-red-500 text-xs italic  text-start'>{errors[field.name]}</p>}
+						</div>
+					))}
+					<div className='flex justify-around place-items-center'>
+						<button
+							disabled={loading}
+							type='submit'
+							className={`flex justify-center items-center bg-slate-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline transition duration-300 ease-in-out hover:scale-105`}
+						>
+							{loading ? (
+								<svg
+									className='animate-spin h-6 w-6 text-white'
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+								>
+									<circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
+									<path
+										className='opacity-75'
+										fill='currentColor'
+										d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+									></path>
+								</svg>
+							) : (
+								<Icon
+									className='cursor-pointer transition duration-300 ease-in-out hover:scale-110'
+									icon='bi:send-fill'
+									width={25}
+									height={25}
+								/>
+							)}
+						</button>
+						<a className='flex items-center gap-10' href={`https://wa.link/m7zcha`} target='_blank'>
+							<Icon
+								className='cursor-pointer transition duration-300 ease-in-out hover:scale-110'
+								icon='logos:whatsapp-icon'
+								width={40}
+								height={40}
+							/>
+						</a>
+					</div>
+				</form>
+			</div>
 		</div>
-	  );
-	};
+	);
+};
 
 export default ContactForm;
--- components/sections/contact/index.tsx
+++ components/sections/contact/index.tsx
@@ -3,7 +3,7 @@ import ContactForm from './form';
 const Contact = () => {
 	return (
 		<div
-			className='h-full w-full text-white flex justify-center place-items-center rounded-md dark:opacity-90'
+			className='h-screen w-full text-white flex justify-center place-items-center rounded-md dark:opacity-90'
 			style={{
 				backgroundImage: `url(${'/images/contact/contact-background.png'})`,
 				backgroundSize: 'cover',
